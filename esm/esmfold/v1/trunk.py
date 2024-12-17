9cae13f5be074fc1e6efe40debe8c17a39fb25ad
Cizhang Zhao <cizhangzhao@dyn-128-104-118-44.biochem.wisc.edu>
2024-12-17 10:20:14 -0600

    testdrop4

--- esm/esmfold/v1/trunk.py
+++ esm/esmfold/v1/trunk.py
@@ -130,8 +130,8 @@ class FoldingTrunk(nn.Module):
                     pairwise_state_dim=c_z,
                     sequence_head_width=self.cfg.sequence_head_width,
                     pairwise_head_width=self.cfg.pairwise_head_width,
-                    dropout=self.cfg.dropout,
-                    # dropout=0.3,
+                    # dropout=self.cfg.dropout,
+                    dropout=0.1,
                 )
                 for i in range(self.cfg.num_blocks)
             ]
