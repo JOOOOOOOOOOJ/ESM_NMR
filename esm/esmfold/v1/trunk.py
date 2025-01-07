eae0270e7c54ac2d927833ed9c8189e90403c2a8
Cizhang Zhao <cizhangzhao@CizhangdeMacBook-Air.local>
2025-01-06 21:04:27 -0600

    no_change

--- esm/esmfold/v1/trunk.py
+++ esm/esmfold/v1/trunk.py
@@ -132,8 +132,8 @@ class FoldingTrunk(nn.Module):
                     pairwise_state_dim=c_z,
                     sequence_head_width=self.cfg.sequence_head_width,
                     pairwise_head_width=self.cfg.pairwise_head_width,
-                    # dropout=self.cfg.dropout,
-                    dropout=0.25,
+                    dropout=self.cfg.dropout,
+                    # dropout=0.25, JOJO's changes
                 )
                 for i in range(self.cfg.num_blocks)
             ]
@@ -145,9 +145,9 @@ class FoldingTrunk(nn.Module):
         self.recycle_disto = nn.Embedding(self.recycle_bins, c_z)
         self.recycle_disto.weight[0].detach().zero_()
         #JO: Test
-        self.cfg.structure_module.dropout_rate = 0.2
+        # self.cfg.structure_module.dropout_rate = 0.2
         self.structure_module = StructureModule(**self.cfg.structure_module)  # type: ignore
-        print(self.cfg.structure_module)
+        # print(self.cfg.structure_module)
         self.trunk2sm_s = nn.Linear(c_s, self.structure_module.c_s)
         self.trunk2sm_z = nn.Linear(c_z, self.structure_module.c_z)
 
